d8e405521acb55f74f0b3dbb32226841a5dc1089
Prasikcz <dominik.pr@seznam.cz>
2023-03-08 10:20:53 +0100

    4.03

--- evidence_pojisteni/urls.py
+++ evidence_pojisteni/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 
@@ -20,5 +23,8 @@ urlpatterns = [
     path('delete_klient/<str:pk>', views.deleteKlient, name='delete_klient'),
     path('delete_produkt/<str:pk>', views.deleteProdukt, name='delete_produkt'),
    
-
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset_password/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#SMTP CONFIGURATION
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.seznam.cz'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
